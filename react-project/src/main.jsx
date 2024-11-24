0c03b39d9e9435f22316edbf9645b5ddde228866
Maganti <1647781+magantisurya@users.noreply.github.com>
2024-11-24 19:48:36 +0000

    Destructuring Arrays in main.jsx

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const [, , thirdAnimal] = ['Giraffe','Lion','Tiger']
+console.log(thirdAnimal);
+
+// const [firstAnimal, secondAnimal, thirdAnimal] = ['Giraffe','Lion','Tiger']
+// console.log(firstAnimal);
+// console.log(thirdAnimal);
+
+// const animals = ['giraffe','elephant','Lion']
+// console.log(animals[0]);
+
 createRoot(document.getElementById("root")).render(<App />);
